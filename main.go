e06876c1516fee3c9a725d3fe7283a13361496f3
t1bur1an <t1bur1an@gmail.com>
2025-07-01 10:17:06 +0300

    add filter functions, add delete pod function

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/t1bur1an/k8s-pod-ttl-killer/utils"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -21,13 +23,11 @@ func main() {
 	}
 	flag.Parse()
 
-	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
@@ -36,11 +36,18 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 	for i, pod := range pods.Items {
+		podReadyTimestamp, err := utils.GetPodReadyTime(pod)
+		if err != nil {
+			fmt.Printf("Pod %s not in ready state", pod.Spec.Containers[0].Name)
+		}
 		for _, podCondition := range pod.Status.Conditions {
-			if podCondition.Type == "Ready" {
-				fmt.Printf("Pod %d pod time info, annotations: %v: %v\n", i, podCondition.LastTransitionTime, pod.Annotations)
+			if podCondition.Type == corev1.PodReady {
+				ttl, err := utils.FilterAnnotations(pod.Annotations, "pod-killer-ttl")
+				if err != nil {
+					fmt.Printf("Got an error with annotations: %s", err.Error())
+				}
+				fmt.Printf("Pod %d pod time info, timestamp: %d, ttl: %d\n", i, podReadyTimestamp, ttl)
 			}
 		}
 	}
---
+++ utils/utils.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"strconv"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func FilterAnnotations(filterAnnotations map[string]string, targetAnnotation string) (time.Duration, error) {
+	outputDuration := time.Duration(0)
+	for annotation, value := range filterAnnotations {
+		if annotation == targetAnnotation {
+			intValue, err := strconv.Atoi(value)
+			if err != nil {
+				return outputDuration, err
+			}
+			outputDuration = time.Duration(intValue)
+			break
+		}
+	}
+	return outputDuration, errors.New("No valuable annotation found")
+}
+
+func GetPodReadyTime(pod corev1.Pod) (int64, error) {
+	for _, podCondition := range pod.Status.Conditions {
+		if podCondition.Type == corev1.PodReady {
+			return podCondition.LastTransitionTime.Unix(), nil
+		}
+	}
+	return time.Time{}.Unix(), errors.New("Pod is not ready")
+}
+
+func DeletePod(clientset kubernetes.Clientset, namespaceName string, podContext context.Context, containerName string) {
+	err := clientset.CoreV1().Pods(namespaceName).Delete(podContext, containerName, *metav1.NewDeleteOptions(0))
+					if err != nil {
+									panic(err)
+					}
+}
